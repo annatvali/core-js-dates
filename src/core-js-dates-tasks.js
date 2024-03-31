cffd1fdd3991f74a79a04015f7e0d7a0eb4f876c
annatvali <anntvaliashvili@gmail.com>
2024-03-31 23:56:55 +0400

    feat: implement getTime function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,8 +31,17 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  function formatNumber(num) {
+    return num < 10 ? `0${num}` : num;
+  }
+
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  return `${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(
+    seconds
+  )}`;
 }
 
 /**
