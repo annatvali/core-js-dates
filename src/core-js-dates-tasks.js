a894dfd19f15a65f1855607744436792ab6961d3
annatvali <anntvaliashvili@gmail.com>
2024-04-01 00:09:04 +0400

    feat: implement getDayName function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -55,8 +55,19 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const parsedDate = new Date(date);
+  const dayIndex = parsedDate.getUTCDay();
+  return weekDays[dayIndex];
 }
 
 /**
