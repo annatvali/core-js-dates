edc6eb9f75bc797e18cb50ea18a37d9bc3816b11
annatvali <anntvaliashvili@gmail.com>
2024-04-01 00:32:04 +0400

    feat: implement getNextFriday function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -81,8 +81,10 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const day = date.getDay();
+  const daysUntilNextFriday = day === 5 ? 7 : (5 - day + 7) % 7;
+  return new Date(date.getTime() + daysUntilNextFriday * 24 * 60 * 60 * 1000);
 }
 
 /**
